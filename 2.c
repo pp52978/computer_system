bfdde2d8537e1be42d023dda49ef5d854e452aa6
pp <631843571@qq.com>
2022-10-20 22:06:20 +0800

    10/20

--- 2.c
+++ 2.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    char str[120]="\0";
+    char str[120];
     gets(str);
     int k=0;
     while(str[k]!=0)
     {
-        if(str[k]!=' ')
+        if(str[k]>=97&&str[k]<=122)
+        printf("%c",str[k]-32);
+        else
         printf("%c",str[k]);
         k++;
     }
